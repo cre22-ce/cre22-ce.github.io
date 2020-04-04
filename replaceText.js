a2de66f4ad00c118b7a18143fd58f31d038c419a
cre22-ce <cre22@students.uwf.edu>
2020-04-03 23:40:11 -0500

    Actual final. Also updated the main page with proper download instructions

--- replaceText.js
+++ replaceText.js
@@ -1,12 +1,11 @@
 var replaceText = {
-	"main": "file:./main.html",
+    "main": "file:./main.html",
     "another_page": "file:./another_page.html",
     "artifacts": "file:./artifacts.html",
     "reflection": "file:./reflection.html",
-    "research": "file:./research.html",
     "bib": "file:./bib.html",
-    "alltext": "file:./another_page.html",
-	
-	
+    "alltext": "file:./files/All Text.html",
+
+
     "draft": "file:./draft.html",
 }
